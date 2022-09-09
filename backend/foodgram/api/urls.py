51398bd34290ca00290d795e7fa8ba22d28b8fb1
Max Tsyos <oddlingmf@gmail.com>
2022-09-09 17:34:04 +0300

    fixed urls.py

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -10,7 +10,6 @@ router_v1.register(r'tags', TagViewSet, basename='tag')
 router_v1.register(r'ingredients', IngredientViewSet, basename='ingredient')
 
 urlpatterns = [
-    path('v1/', include('djoser.urls')),
-    path('v1/auth/', include('djoser.urls.jwt')),
-    path('v1/', include(router_v1.urls)),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('', include(router_v1.urls)),
 ]
